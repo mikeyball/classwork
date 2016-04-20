f7c4867a092a35cb3a79e66416b981e986424323
mikeyball <mgball@gmail.com>
2016-04-20 15:49:21 -0400

    created first files

---
+++ class/temperature.py
@@ -0,0 +1,29 @@
+
+def main():
+    q=True
+    while(q==True):
+        x=ftemp()
+        ctemp(x)
+        q=False
+
+def ftemp():
+    return int(input("Enter the temperature in Fahrenheit"))
+
+def ctemp(a):
+    temp=(a-32)/1.8
+    print(temp,"C")
+    print(a,"F")
+    go=str(input("Would you Like to continue?"))
+    if go=="yes":
+        print("Let's go again")
+        x=ftemp()
+        ctemp(x)
+    if go=="no":
+        print("goodbye")
+
+main()
+
+
+
+
+
